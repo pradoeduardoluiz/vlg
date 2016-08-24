ee9cea447da2c09ff36a8a321c73ee47ef155e71
=Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2016-08-23 22:33:43 -0300

    Incluido classe emailutil que envia e-mails usando configuracao web.xml

---
+++ vlg/src/br/com/vlg/sistemaweb/util/ContactMB.java
@@ -0,0 +1,70 @@
+
+package br.com.vlg.sistemaweb.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.mail.MessagingException;
+
+@ManagedBean
+public class ContactMB {
+
+	private String fullName;
+	private String phone;
+	private String mail;
+	private String message;
+
+	public String enviar() {
+
+		try {
+			EmailUtil.sendMail(mail, "Contact", message);
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "E-mail", "Enviado com sucesso!!"));
+
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "E-mail", "Houve um problema no envio!!"));
+
+			e.printStackTrace();
+		}
+
+		return "";
+
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ vlg/src/br/com/vlg/sistemaweb/util/EmailUtil.java
@@ -0,0 +1,84 @@
+package br.com.vlg.sistemaweb.util;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailUtil {
+
+	public static void sendMail(String to, String subject, String body) throws MessagingException {
+
+		Authenticator authenticator = getAuthenticator();
+		InternetAddress mailFrom = new InternetAddress(getFrom());
+		InternetAddress mailTo = new InternetAddress(to);
+		Properties properties = getProperties();
+
+		Session session = Session.getDefaultInstance(properties, authenticator);
+
+		Message msg = new MimeMessage(session);
+
+		msg.setFrom(mailFrom);
+		msg.setSentDate(new Date());
+
+		msg.setRecipient(Message.RecipientType.TO, mailTo);
+		msg.setSubject(subject);
+
+		msg.setText(body);
+
+		Transport.send(msg);
+
+	}
+
+	private static Properties getProperties() {
+
+		ExternalContext context = getContext();
+
+		Properties properties = new Properties();
+
+		properties.put("mail.smtp.host", context.getInitParameter("servidor"));
+		properties.put("mail.smtp.port", context.getInitParameter("porta"));
+		properties.put("mail.smtp.auth", context.getInitParameter("auth"));
+		properties.put("mail.smtp.starttls.enable", context.getInitParameter("tls"));
+
+		return properties;
+
+	}
+
+	private static ExternalContext getContext() {
+
+		return FacesContext.getCurrentInstance().getExternalContext();
+	}
+
+	private static String getFrom() {
+
+		ExternalContext context = getContext();
+
+		return context.getInitParameter("usuario");
+	}
+
+	private static Authenticator getAuthenticator() {
+
+		final ExternalContext context = getContext();
+
+		return new Authenticator() {
+
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(context.getInitParameter("usuario"),
+						context.getInitParameter("senha"));
+			}
+
+		};
+
+	}
+
+}
